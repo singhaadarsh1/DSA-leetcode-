1bd3a5f452f4592427e95571d32ccd39b45b9772
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-03-21 13:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (78.06%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,18 +4,18 @@ public:
         int s = 0;
         int e = arr.size() - 1;
 
-        while (s < e) {
+        while (s <= e) {
             int mid = s + (e - s) / 2;
 
             if (arr[mid] < arr[mid + 1]) {
-                // increasing part → move right
+            
                 s = mid + 1;
             } else {
-                // decreasing part → peak is at mid or left
-                e = mid;
+            
+                e = mid-1;
             }
         }
 
-        return s; // or return e (both same here)
+        return s; 
     }
 };
