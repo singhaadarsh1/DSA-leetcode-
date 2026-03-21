1e3bc432b4142e4e8e22e92a8e58a1e01667fe6a
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-03-21 18:53:43 +0530

    Delete 383-ransom-note directory

--- 383-ransom-note/ransom-note.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        s1 = Counter(ransomNote)
-        s2 = Counter(magazine)
-        if s1<=s2:
-            return True
-        else:
-            return False    
