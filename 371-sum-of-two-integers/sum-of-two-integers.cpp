c8604d85c3d41f1c1c747483bbf31ed0833cec6c
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-03-29 17:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (1.69%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int getSum(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
 
-return a+b;
+            a = sum;
+            b = carry;
+        }
+        return a;
+
+
     }
 };
