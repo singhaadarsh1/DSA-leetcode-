ea4396e5fd15f9567982907d67808e90a5f9e258
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-03-31 20:41:27 +0530

    Time: 1 ms (36.85%) | Memory: 9.2 MB (58.81%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string result = "";
+    int i = num1.size() - 1;
+    int j = num2.size() - 1;
+    int carry = 0;
+
+    while (i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+
+        if (i >= 0) {
+            sum += num1[i] - '0';
+            i--;
+        }
+
+        if (j >= 0) {
+            sum += num2[j] - '0';
+            j--;
+        }
+
+        result.push_back((sum % 10) + '0');
+        carry = sum / 10;
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+    }
+};
