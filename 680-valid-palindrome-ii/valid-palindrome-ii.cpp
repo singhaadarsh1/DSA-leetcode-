443112560db3876c2099adacf40ecd7f62706f38
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-04-07 22:38:05 +0530

    Time: 4 ms (58.85%) | Memory: 18.7 MB (98.92%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool ispalindrome(string& s, int l, int r) {
+        while (l <= r) {
+            if (s[l] != s[r]) {
+                return false;
+            } else {
+                l++;
+                r--;
+            }
+            
+        }
+        return true;
+    }
+    bool validPalindrome(string& s) {
+        int n = s.size();
+        int st = 0;
+        int e = n - 1;
+        while (st < e) {
+            if (s[st] != s[e]) {
+                return (ispalindrome(s, st + 1, e) ||
+                        ispalindrome(s, st, e - 1));
+            }
+            st++;
+            e--;
+        }
+        return true;
+    }
+};
