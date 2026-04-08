22c631cb8ebb99b649550a341a2c8c387c853b5c
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-04-08 16:40:39 +0530

    Time: 3 ms (64.91%) | Memory: 7.8 MB (37.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    int fib(int n) {
-        if (n == 0) {
+    int fibTabSpaceOptimized(int n) {
+        int prev = 0;
+        if (n == 0)
             return 0;
-        } else if (n == 1) {
-            return 1;
+        int curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int ans = prev + curr;
+            prev = curr;
+            curr = ans;
         }
-        return fib(n - 1) + fib(n - 2);
+        return curr;
+    }
+    int fib(int n) {
+        /*   if (n == 0) {
+               return 0;
+           } else if (n == 1) {
+               return 1;
+           }
+           return fib(n - 1) + fib(n - 2);*/
+
+        return fibTabSpaceOptimized(n);
     }
 };
