0379c8c65125f653b44188df771ca0788cdb659a
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-04-09 17:07:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (47.66%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char, int> freq;
+        //unordered_map<char, int> freq;
         unordered_map<char, int> freq1;
-        for (char num : jewels) {
-            freq[num]++;
-        }
+       // for (char num : jewels) {
+            //freq[num]++;
+       // }
         for (char num : stones) {
             freq1[num]++;
         }
         int count=0;
-        for(auto &it:freq){
+        for(char ch :jewels){
             for(auto &itt:freq1){
-                if(it.first==itt.first){
+                if(ch==itt.first){
                     count+=itt.second;
                 }
             }
