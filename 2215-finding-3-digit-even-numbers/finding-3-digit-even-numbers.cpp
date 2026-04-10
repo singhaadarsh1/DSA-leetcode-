13392dd231f633d4d8b43a299ad852ff8a490d4d
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-04-10 23:16:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (89.72%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        set<int> res;
+        /*set<int> res;
         int num;
         for (int i = 0; i < digits.size(); i++) {
             for (int j = 0; j < digits.size(); j++) {
@@ -9,14 +9,45 @@ public:
                     if (i == j || j == k || k == i) {
                         continue;
                     }
-                    num = (digits[i] * 100) + (digits[j] * 10) + (digits[k] * 1);
-                    if(num>=100 && num%2==0){
-                        res.insert(num);
+                    num = (digits[i] * 100) + (digits[j] * 10) + (digits[k] *
+       1); if(num>=100 && num%2==0){ res.insert(num);
                     }
                 }
             }
         }
        vector<int>st(res.begin(),res.end());
-        return st;
+        return st;*/
+        unordered_map<int, int> freq;
+        vector<int> result;
+        int nums;
+
+        for (int num : digits) {
+            freq[num]++;
+        }
+        
+        for (int i = 1; i <= 9; i++) {
+            if (freq[i] == 0)
+                continue;
+            freq[i]--;
+
+            for (int j = 0; j <= 9; j++) {
+                if (freq[j] == 0)
+                    continue;
+                freq[j]--;
+
+                for (int k = 0; k <= 8; k += 2) {
+                    if (freq[k] == 0)
+                        continue;
+                    freq[k]--;
+                    nums =
+                        (i * 100) + (j * 10) + (k * 1);
+                    result.push_back(nums);
+                    freq[k]++;
+                }
+                freq[j]++;
+            }
+            freq[i]++;
+        }
+        return result;
     }
 };
