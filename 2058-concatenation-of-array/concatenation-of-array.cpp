2f3363eb5f1b389880faa759c610198173e3a4d5
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-04-18 15:40:30 +0530

    Time: 3 ms (10.73%) | Memory: 17.2 MB (15.79%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-       /* string ans = "";
-        for (int c : nums) {
-            ans+=c;
+        /* string ans = "";
+         for (int c : nums) {
+             ans+=c;
+         }
+         string s = "";
+         for (int i = 0; i < nums.size(); i++) {
+             s+=(nums[i]);
+         }
+         string temp = ans + s;
+         vector<int> res;
+         for (int i = 0; i < temp.size(); i++) {
+             res.push_back(temp[i]);
+         }
+         return res;
+         vector<int>res(2n)
+         for(int x:nums){
+             res.push_back(x);
+         }
+         for(int x:nums){
+             res.push_back(x);
+         }
+         return res;*/
+        int n = nums.size();
+        vector<int> ans;
+        for (int i = 0; i < 2* n; i++) {
+            if (i < n) {
+                ans.push_back(nums[i]);
+            } else {
+                ans.push_back(nums[i - n]);
+            }
         }
-        string s = "";
-        for (int i = 0; i < nums.size(); i++) {
-            s+=(nums[i]);
-        }
-        string temp = ans + s;
-        vector<int> res;
-        for (int i = 0; i < temp.size(); i++) {
-            res.push_back(temp[i]);
-        }
-        return res;*/
-        vector<int>res;
-        for(int x:nums){
-            res.push_back(x);
-        }
-        for(int x:nums){
-            res.push_back(x);
-        }
-        return res;
+        return ans;
     }
 };
