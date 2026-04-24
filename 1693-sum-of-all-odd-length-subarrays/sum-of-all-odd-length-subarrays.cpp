711b6d2ce14d117cc98ac8dc59089286430f1fcb
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-04-24 09:45:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (82.16%) - LeetSync

---
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+
+        int total = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            int sum = 0;
+            for (int j = i; j < arr.size(); j++) {
+                sum += arr[j];
+
+                if ((j - i) % 2 == 0) {
+                    total += sum;
+                }
+            }
+        }
+        return total;
+    }
+};
