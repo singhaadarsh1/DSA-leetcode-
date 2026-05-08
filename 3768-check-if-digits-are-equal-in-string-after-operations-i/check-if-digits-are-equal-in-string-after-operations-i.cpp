328ab887e1e25d265b584d91cacf5c10a9662496
aadarsh singh <aadarshsinghsikriwal153@gmail.com>
2026-05-08 21:35:55 +0530

    Time: 1 ms (83.41%) | Memory: 8.4 MB (92.86%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -4,7 +4,7 @@ public:
     
         while(s.size()!=2){
             for(int i=0;i<s.size()-1;i++){
-                s[i]=(s[i]+s[i+1])%10;
+                s[i]=(s[i]-'0'+s[i+1]-'0')%10;
             }
             s.pop_back();
             if(s.size()==2 && s[0]==s[1]){
