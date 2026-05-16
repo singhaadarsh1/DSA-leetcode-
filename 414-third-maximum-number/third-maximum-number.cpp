2dfb4221c90c8a10e2a890f774af39d454f4ddea
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-05-16 18:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (50.17%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int> st;
-        for (int n : nums) {
-            st.insert(n);
-        }
-        vector<int> v(st.begin(), st.end());
-        sort(v.begin(),v.end());
-        int maxi = INT_MIN;
-        for (int n:v) {
-            maxi = max(maxi, n);
-        }
-        if (v.size() < 3) {
-            return maxi;
-        }
+        /* unordered_set<int> st;
+         for (int n : nums) {
+             st.insert(n);
+         }
+         vector<int> v(st.begin(), st.end());
+         sort(v.begin(),v.end());
+         int maxi = INT_MIN;
+         for (int n:v) {
+             maxi = max(maxi, n);
+         }
+         if (v.size() < 3) {
+             return maxi;
+         }
+
+         int count = 1;
+         for (int i = v.size() - 1; i >= 0; i--) {
+             if (count == 3) {
+                 return v[i];
+             } else {
+                 count++;
+             }
+         }
+         return 0;*/
+         long l = LONG_MIN;
+        long s = LONG_MIN;
+        long t = LONG_MIN;
+        int n = nums.size();
         
-        int count = 1;
-        for (int i = v.size() - 1; i >= 0; i--) {
-            if (count == 3) {
-                return v[i];
+      
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > l) {
+                t = s; s = l; l = nums[i];
+            } else if (nums[i] > s && nums[i] != l) {
+                t = s;
+                s = nums[i];
+            } else if (nums[i] > t && nums[i] != s && nums[i]!=l) {
+                t = nums[i];
             } else {
-                count++;
+                continue;
             }
         }
-        return 0;
+        return (t==LONG_MIN)?l:t;
+        
     }
 };
