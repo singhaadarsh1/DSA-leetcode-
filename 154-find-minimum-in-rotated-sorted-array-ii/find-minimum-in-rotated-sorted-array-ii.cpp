2046cdcff10a0fc3ef0e6e911480ff2970817145
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-05-17 15:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (89.93%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int mini=INT_MAX;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            mini=min(mini,nums[i]);
-        }
-       // for(int i=0;i<n;i++){
-           // if(nums[i]==mini&& i==0){
-               // return nums[i];
-           // }
-            int idx = min_element(nums.begin(), nums.end()) - nums.begin();
-            int k=idx-0;
-            rotate(nums.begin(),nums.begin()+k,nums.end());
-
-            
-        //}
-        return nums[0];
-    
+        //brute force approach,tc=o(n)
+       /* int n = nums.size();
         
-    
+        
+        int idx = min_element(nums.begin(), nums.end()) - nums.begin();
+        int k = idx - 0;
+        rotate(nums.begin(), nums.begin() + k, nums.end());
+
+        
+        return nums[0];*/
+        //optimized(binary search approach),tc->o(logn)
+         int n=nums.size();
+        int s = 0;
+        int e = nums.size() - 1;
+        int mid = s + (e - s) / 2;
+        while (s < e) {
+            if (nums[mid] <nums[n-1]) {
+                e = mid;
+            } else if (nums[mid] >nums[n-1]) {
+                s=mid+1;
+            }else if(nums[mid]==nums[n-1]&&nums[s]<nums[n-1]){
+                e--;
+            }
+            else if(nums[mid]==nums[n-1]){
+                s++;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return nums[s];
+        
         
     }
 };
