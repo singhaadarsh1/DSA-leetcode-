11d40cf93bb052499f7a462f0761822aad496f26
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-05-17 10:07:08 +0530

    Time: 733 ms (5.14%) | Memory: 8.8 MB (39.98%) - LeetSync

---
+++ 4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    //long long power(long long x, int k) {
+      //  long long ans = 1;
+       // while (k--) {
+            //ans *= x;
+       // }
+       // return ans;
+   // }
+    int countKthRoots(int l, int r, int k) {
+        if (k == 1) {
+            int count = 0;
+            int i = l;
+            while (i <= r) {
+                count++;
+                i++;
+            }
+            return count;
+        }
+        int count = 0;
+        long long x = 0;
+        while (true) {
+            long long value = pow(x, k);
+            if (value > r) {
+                break;
+            } else if (value >= l) {
+                count++;
+            }
+            x++;
+        }
+        return count;
+    }
+};
