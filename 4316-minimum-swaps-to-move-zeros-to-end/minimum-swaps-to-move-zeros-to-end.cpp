4557e756d3c09286e0cc8dcce830c57025174186
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-05-24 00:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (58.91%) - LeetSync

--- 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
+++ 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
@@ -8,7 +8,7 @@ public:
         int i = 0;
         int j = n - 1;
         int count = 0;
-        while (i < j && j < n) {
+        while (i < j ) {
             if (nums[i] == 0 && nums[j] == 0) {
                 j--;
             } else if (nums[i] == 0) {
