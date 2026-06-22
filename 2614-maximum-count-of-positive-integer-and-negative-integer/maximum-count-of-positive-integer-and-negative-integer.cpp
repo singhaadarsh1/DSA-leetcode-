568e75536e055aca25d282cba07366e7aa60ca2d
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-06-22 15:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (89.43%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int mini=INT_MIN;
-        int maxi=INT_MAX;
-        int ncnt=0;
+        // brute force approach , tc-o(n);
+
+        /*int ncnt=0;
         int pcnt=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]<0){
@@ -14,7 +14,21 @@ public:
                 continue;
             }
         }
-        return max(ncnt,pcnt);
+        return max(ncnt,pcnt);*/
+
+        // binary search using lower_bound and upper_bound, tc-o(logn);
+        auto x = (lower_bound(nums.begin(), nums.end(), 0));
+        auto y = (upper_bound(nums.begin(), nums.end(), 0));
+        int n;
+        int p;
         
+        if (x - nums.begin() == y - nums.begin()) {
+            n = (x - nums.begin()) - 0;
+            p = nums.size() - (x - nums.begin());
+        } else {
+            p = nums.size()- (y - nums.begin());
+            n = (x - nums.begin()) - 0;
+        }
+        return max(n, p);
     }
 };
