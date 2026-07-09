3f8179c959c84b4acfd9ebdaf55e37e89a67f5d1
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-07-09 18:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (74.25%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long sumAndMultiply(int n) {
 
-        long long nonzeroes = 0;
+        /*long long nonzeroes = 0;
         vector<int> res;
         int digit;
         while (n) {
@@ -20,6 +20,23 @@ public:
             finaldigit +=  res[i] * val;
             val *= 10;
         }
-        return finaldigit * nonzeroes;
+        return finaldigit * nonzeroes;*/
+         long long sum = 0;
+        long long num = 0;
+        long long place = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit != 0) {
+                sum += digit;
+                num += 1LL * digit * place;
+                place *= 10;
+            }
+
+            n /= 10;
+        }
+
+        return num * sum;
     }
 };
