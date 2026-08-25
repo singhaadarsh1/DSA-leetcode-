2d576c2dd8d7a5d55caa48384c244081840918ad
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-08-25 17:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (24.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        /* vector<int> arr;
-         for (int i = 0; i < nums.size(); i++) {
-             if (nums[i] % 2 == 0) {
-                 arr.push_back(nums[i]);
+
+        /* for (int i = 1; i <= nums.size()+1 ; i++) {
+             if (find(nums.begin(), nums.end(), k * i) == nums.end()) {
+                 return k * i;
+             } else {
+                 continue;
              }
          }
-         int count = 1;*/
-        for (int i = 1; i <= nums.size()+1 ; i++) {
-            if (find(nums.begin(), nums.end(), k * i) == nums.end()) {
+         return 0;*/
+        unordered_set<int> st;
+        for (int n : nums) {
+            st.insert(n);
+        }
+        int i = 1;
+        for (auto & it : st) {
+
+            if (st.find(k * i) == st.end()) {
                 return k * i;
+
             } else {
+                i++;
                 continue;
             }
+            
         }
-        return 0;
+        return k*i;
     }
 };
